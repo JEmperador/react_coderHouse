0e254c9b7af1ad303ba78dd537e7bf3076c794bc
jemperador <javier_emperador@outlook.com>
2022-07-21 22:09:16 -0300

    Desafio 3 | Contador al lado de CartWidget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,11 +1,15 @@
 import carrito from "../../assets/images/cart.svg";
+import { Badge } from "react-bootstrap";
 import "./CartWidget.css";
 
 function CartWidget() {
   return (
-    <>
+    <div>
+      <Badge className="fs-6 contador" pill bg="primary">
+        0
+      </Badge>
       <img src={carrito} alt="carrito" className="carrito" />
-    </>
+    </div>
   );
 }
 
