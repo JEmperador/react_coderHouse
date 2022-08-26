f5771ee4ff0d6887db88e308116e37105dc09a8c
jemperador <javier_emperador@outlook.com>
2022-08-26 19:00:33 -0300

    orden, env

--- src/containers/ItemListContainer/ItemListContainer.js
+++ src/containers/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Delay from "../../components/Delay/Delay";
 import ItemList from "../../components/ItemList/ItemList";
-import { getDocs, collection, query, where } from "firebase/firestore";
+import { getDocs, collection, query, where, orderBy } from "firebase/firestore";
 import { db } from "../../service/firebase";
 import { useParams } from "react-router-dom";
 import "./ItemListContainer.css";
@@ -12,14 +12,16 @@ function ItemListContainer(props) {
   const { categoryId } = useParams();
 
   useEffect(() => {
-    const ref = collection(db, "products");
+    const ref = query(collection(db, "products"))
+    const orderRef = query(collection(db, "products"), orderBy("order"));
 
-    const queryTipe = !categoryId ? ref : query(ref, where("category", "==", categoryId));
+    const queryTipe = !categoryId ? orderRef : query(ref, where("category", "==", categoryId));
 
     getDocs(queryTipe)
       .then((response) => {
         const products = response.docs.map((doc) => {
           const data = doc.data();
+          console.log(doc);
           return { id: doc.id, ...data };
         });
         setProducts(products);
@@ -32,14 +34,25 @@ function ItemListContainer(props) {
       });
   }, [categoryId]);
 
+  if (products.length === 0) {
+    <h1 className="text-center">Pagina en mantenimiento! vuelve en unos minutos...</h1>;
+  }
+
   return (
     <>
       <h1 className="title">{`${props.title} ${categoryId || ""}`}</h1>
       {!loading && <Delay />}
       {loading && (
-        <div className="productsList mt-5">
-          <ItemList products={products} />
-        </div>
+        <>
+          <div className="productsList mt-5">
+            <ItemList products={products} />
+          </div>
+          <div className="WaLink text-center">
+            <p>
+              ¿Buscas otro producto? escribinos <a href="https://wa.me/5493816176941?text=Hola%20quiero%20consultar%20por%20[escriba%20aqui%20el%20producto%20que%20busca]" target="_blank" rel="noreferrer">aqui</a>
+            </p>
+          </div>
+        </>
       )}
     </>
   );
--- src/service/firebase/index.js
+++ src/service/firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZuwE-lALfvlCEyUglb23mu9rWhPnA660",
-  authDomain: "atlas-tech-cba9a.firebaseapp.com",
-  projectId: "atlas-tech-cba9a",
-  storageBucket: "atlas-tech-cba9a.appspot.com",
-  messagingSenderId: "469292570763",
-  appId: "1:469292570763:web:365301609cd60156cf5c8e"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
